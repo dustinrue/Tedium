07ced604b7e68b5f6b2e1c15d71ee25b410e13f2
Dustin Rue <ruedu@dustinrue.com>
2012-01-16 21:56:55 -0600

    fix copyright line

--- Tedium/AppDelegate.m
+++ Tedium/AppDelegate.m
@@ -3,7 +3,7 @@
 //  Tedium
 //
 //  Created by Dustin Rue on 1/16/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012. All rights reserved.
 //
 
 #import "AppDelegate.h"
