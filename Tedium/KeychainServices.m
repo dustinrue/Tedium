c5a1fa6a3f63b35d3346020823fbb1fb066ec058
Dustin Rue <ruedu@dustinrue.com>
2012-02-10 15:44:26 -0600

    modernization step 1

--- Tedium/KeychainServices.m
+++ Tedium/KeychainServices.m
@@ -24,46 +24,32 @@
 
 + (BOOL)checkForExistanceOfKeychainItem:(NSString *)keychainItemName withItemKind:(NSString *)keychainItemKind forUsername:(NSString *)username withAddress:(NSString *) address
 {
-	SecKeychainSearchRef search;
-	SecKeychainItemRef item;
-	SecKeychainAttributeList list;
-	SecKeychainAttribute attributes[4];
+
+    CFTypeRef results;
     OSErr result;
-    int numberOfItemsFound = 0;
-	
-	attributes[0].tag = kSecAccountItemAttr;
-    attributes[0].data = (void *)[username UTF8String];
-    attributes[0].length = [[NSNumber numberWithUnsignedLong:[username length]] unsignedIntValue];
-    
-    attributes[1].tag = kSecDescriptionItemAttr;
-    attributes[1].data = (void *)[keychainItemKind UTF8String];
-    attributes[1].length = [[NSNumber numberWithUnsignedLong:[keychainItemKind length]] unsignedIntValue];
-	
-	attributes[2].tag = kSecLabelItemAttr;
-    attributes[2].data = (void *)[keychainItemName UTF8String];
-    attributes[2].length = [[NSNumber numberWithUnsignedLong:[keychainItemName length]] unsignedIntValue];
-    
-    attributes[3].tag = kSecServerItemAttr;
-    attributes[3].data = (void *)[address UTF8String];
-    attributes[3].length = [[NSNumber numberWithUnsignedLong:[address length]] unsignedIntValue];
 
-    list.count = 4;
-    list.attr = attributes;
+    NSMutableDictionary *query = [[NSMutableDictionary alloc] init];
+    
+    [query setObject:(id)kSecClassInternetPassword forKey:kSecClass];
+    [query setObject:username forKey:kSecAttrAccount];
+    [query setObject:keychainItemKind forKey:kSecAttrDescription];
+    [query setObject:address forKey:kSecAttrServer];
+    [query setObject:keychainItemName forKey:kSecAttrLabel];
+    [query setObject:(id)kCFBooleanTrue forKey:kSecReturnAttributes];
+    [query setObject:[NSNumber numberWithInt:2] forKey:kSecMatchLimit];
 
-    result = SecKeychainSearchCreateFromAttributes(NULL, kSecInternetPasswordItemClass, &list, &search);
+    result = SecItemCopyMatching((__bridge CFDictionaryRef)query, (CFTypeRef *)&results);
+
+    NSDictionary *resultsAsDictionary = (__bridge_transfer NSDictionary*)results;
 
     if (result != noErr) {
-        NSLog (@"status %d from SecKeychainSearchCreateFromAttributes\n", result);
+        NSLog (@"failed with %d\n", result);
+    }	
+    else {
+        NSLog(@"found %lu items",[resultsAsDictionary count]);
     }
-    
-	while (SecKeychainSearchCopyNext (search, &item) == noErr) {
-        CFRelease (item);
-        numberOfItemsFound++;
-    }
-	
-	NSLog(@"%d items found\n", numberOfItemsFound);
-    CFRelease (search);
-	return numberOfItemsFound;
+
+	return [resultsAsDictionary count];
 }
 
 + (BOOL)deleteKeychainItem:(NSString *)keychainItemName withItemKind:(NSString *)keychainItemKind forUsername:(NSString *)username withAddress:(NSString *)address
